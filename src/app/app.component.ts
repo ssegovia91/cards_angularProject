16b334d250b66c7217d655179ea5c7c4a3f70f3d
Saul Segovia <saul.segovia.hdez@gmail.com>
2021-02-04 15:00:36 -0600

    Initial commit project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: '@nature',
+      content: 'I saw a tree'
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: '@mounter',
+      content: 'I like the mountain'
+    },
+    {
+      title: 'Bike',
+      imageUrl: 'assets/biking.jpeg',
+      userName: '@biker',
+      content: 'I like to do bike'
+    }
+  ]
 }
